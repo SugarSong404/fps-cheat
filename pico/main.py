a32e0f4dadc0f855ea17b0ce196f03beb70d61be
SugarSong404 <131441711+SugarSong404@users.noreply.github.com>
2025-04-17 11:12:45 +0800

    new

--- pico/main.py
+++ pico/main.py
@@ -5,36 +5,9 @@ import time
 import random
 from adafruit_hid.mouse import Mouse
 
-def smooth_move(mouse, target_x, target_y, duration=0.02, overshoot_range=5):
-    start_x, start_y = 0, 0 
-    steps = int(duration / 0.01) 
-
-    overshoot_x = target_x + random.randint(-overshoot_range, overshoot_range)
-    overshoot_y = target_y + random.randint(-overshoot_range, overshoot_range)
-    
-    for i in range(steps):
-        progress = (i + 1) / steps
-        if progress < 0.8:
-            cur_x = start_x + progress * (overshoot_x - start_x)
-            cur_y = start_y + progress * (overshoot_y - start_y)
-        else:
-            cur_x = overshoot_x - (progress - 0.8) * (overshoot_x - target_x) / 0.2
-            cur_y = overshoot_y - (progress - 0.8) * (overshoot_y - target_y) / 0.2
-        
-        dx = int(cur_x - start_x)
-        dy = int(cur_y - start_y)
-        
-        mouse.move(dx, dy)
-        start_x, start_y = cur_x, cur_y
-        time.sleep(0.01)
-    
-    final_dx = target_x - start_x
-    final_dy = target_y - start_y
-    mouse.move(final_dx, final_dy)
-
-
-uart = busio.UART(board.GP4, board.GP5, baudrate=9600)
+uart = busio.UART(board.GP4, board.GP5, baudrate=115200)
 mouse = Mouse(usb_hid.devices)
+first_control = False
 
 while True:
     data = uart.readline()
@@ -42,26 +15,19 @@ while True:
         try:
             message = data.decode("utf-8").strip()
             print(f"Received data: {message}")
-            
             if message and "," in message:
-                x, y = map(int, message.split(","))
-                
-                if x == 0 and y == 0:
-                    print("No target detected, skipping movement.")
-                    continue
-                elif x == 9999 and y == 9999:
+                x, y = map(int, message.split(","))                    
+                if x==9999 and y==9999:
+                    mouse.release(Mouse.LEFT_BUTTON)
+                    first_control = False
+                elif x==8888 and y==8888:
                     mouse.click(Mouse.LEFT_BUTTON)
-                    print("Mouse clicked! Be triggerred success!")
-                    continue
-                
-                mouse.move(x, y)
-                
-                mouse.click(Mouse.LEFT_BUTTON)
-                print(f"Mouse moved smoothly to {x}, {y} and clicked.")
-                
-            else:
-                print("Error: Invalid or empty data received")
-                
+                    first_control = False
+                else:
+                    if not first_control:
+                        mouse.press(Mouse.LEFT_BUTTON)
+                    first_control = True
+                    mouse.move(x,y)
         except Exception as e:
             print(f"Error: Failed to decode or parse the received data. Exception: {e}")
     else:
